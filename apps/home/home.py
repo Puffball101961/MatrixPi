8c2697c8b357e1afddef94aa3536faf6d9e42c7d
Joel Muscat <37782411+Puffball101961@users.noreply.github.com>
2024-07-20 20:53:08 +1000

    Update home

--- apps/home/home.py
+++ apps/home/home.py
@@ -1,15 +1,20 @@
-# Dummy home.py file for testing
-# python home.py rows cols mapping colourMap
+# Home App
 
-from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
-# from RGBMatrixEmulator import RGBMatrix, RGBMatrixOptions, graphics
+# from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
+from RGBMatrixEmulator import RGBMatrix, RGBMatrixOptions, graphics
 from PIL import Image, ImageFont, ImageDraw, ImageOps, ImageFilter
 import sys
 import time
 
-rows = int(sys.argv[2])
-cols = int(sys.argv[3])
-mapping = sys.argv[4]
+# rows = int(sys.argv[2])
+# cols = int(sys.argv[3])
+# mapping = sys.argv[4] # UNCOMMENT FOR PROD
+
+
+# TEMP
+rows = 32
+cols = 128
+mapping = 'adafruit-hat'
 
 options = RGBMatrixOptions()
 options.rows = rows # height of the display
@@ -20,15 +25,18 @@ options.hardware_mapping = mapping
 
 matrix = RGBMatrix(options = options)
 
-nameFont = ImageFont.load("./apps/home/pil/6x10.pil")
-
-splash = Image.new('RGBA', (cols, rows))
-draw = ImageDraw.Draw(splash)
-draw.text((1,0), "Home", font=nameFont, fill=(255,255,255))
-matrix.SetImage(splash.convert('RGB'))
-
 # Main loop
 while True:
-    time.sleep(1)
+    image = Image.new('RGBA', (128,32))
+    draw = ImageDraw.Draw(image)
+    
+    
+    draw.text((1,0), time.strftime("%I:%M %p"), font=ImageFont.load("./fonts/10x20.pil"), fill=(255,255,255))
+    draw.text((1,20), time.strftime("%a %d/%m/%Y"), font=ImageFont.load("./fonts/6x10.pil"), fill=(255,255,255))
+    
+    matrix.Clear()
+    matrix.SetImage(image.convert('RGB'))
+    
+    time.sleep(60-time.localtime().tm_sec)
 
 matrix.Clear()
